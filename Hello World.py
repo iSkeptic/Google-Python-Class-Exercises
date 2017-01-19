aebc59b5c32c808d2e06d71fc88aa231e65342bc
Fahad <Fahad@MacBooks-MacBook-Pro.local>
2017-01-19 03:05:56 -0800

    Hello World slight modification

--- Hello World.py
+++ Hello World.py
@@ -1,15 +1,10 @@
 import sys
 
-
-def hello(name):
-
-    name += '!!!!'
-    print 'Hello', name
-
+def Hello(name):
+    print "Hello", name
 
 def main():
-
-    hello(sys.argv[1])
+    Hello(sys.argv[1])
 
 
 if __name__ == '__main__':
